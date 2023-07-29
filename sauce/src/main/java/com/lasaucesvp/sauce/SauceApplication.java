8809c87b5726de50bfb6b37f8104b66d54b2e630
TessCope <tj.correz@gmail.com>
2023-07-29 16:16:45 -0400

    Backend Springboot
    Virgin

---
+++ sauce/src/main/java/com/lasaucesvp/sauce/SauceApplication.java
@@ -0,0 +1,13 @@
+package com.lasaucesvp.sauce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SauceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SauceApplication.class, args);
+	}
+
+}
---
+++ sauce/src/test/java/com/lasaucesvp/sauce/SauceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lasaucesvp.sauce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SauceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
